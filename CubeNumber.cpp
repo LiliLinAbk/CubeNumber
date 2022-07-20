540219e8188e5be742bc2262f1e216e7d7788c64
LiliLinAbk <109550088+LiliLinAbk@users.noreply.github.com>
2022-07-20 13:25:07 -0700

    Update CubeNumber.cpp

--- CubeNumber.cpp
+++ CubeNumber.cpp
@@ -4,13 +4,12 @@ int main() {
 
     int number;
     int cube;
-
+    
     cout << "Please Enter a number: ";
     cin >> number;
 
     cube = number * number * number;
-
-
     cout << "The Cube of " << number << " is: " << cube << endl;
+    
     return 0;
 }
